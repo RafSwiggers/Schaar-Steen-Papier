14cafedaaaa30775dd9ce7a038508ebdaec539d7
RafSwiggers <rafswiggers@gmail.com>
2019-06-17 14:15:10 +0200

    add script links and text field input

--- script/javascript.js
+++ script/javascript.js
@@ -6,19 +6,15 @@ function start() {
     firstDiv.classList.add("d-flex");
     startDiv.classList.add("d-none");
     startDiv.classList.remove('d-flex');
+    goButton = document.getElementById("second-game-div");
+    goButton.classList.remove("d-none");
+    goButton.classList.add("d-flex");
 }
 
 function go() {
     // this function brings the start button into play as soon as the user makes their choice.
-    goButton = document.getElementById("second-game-div");
-    goButton.classList.remove("d-none");
-    goButton.classList.add("d-flex");
 
-}
 
-function setvar(x) {
-    // sets the user choice, depending on the button the user clicks
-    userChoice = x;
 }
 
 function delayedGo() {
@@ -26,6 +22,8 @@ function delayedGo() {
 }
 
 function rockPaperScissors() {
+    Choice = document.getElementById("userInput").value;
+    userChoice = Choice.toLowerCase();
     var winText = document.getElementById("end-text");
     var winDiv = document.getElementById("end-div")
     var computerDiv = document.getElementById("computer-choice");
@@ -37,29 +35,33 @@ function rockPaperScissors() {
     var choices = ["nothing", "rock", "paper", "scissors"];
     computerDiv.innerText = "The computer chose " + choices[computerChoice];
     // sets the computer choice from an array of choices for display. 
-    if (userChoice === 1) {
-        if (computerChoice === 2) {
-            winText.innerText = "The winner is: the computer!";
-        } else if (computerChoice === 1) {
-            winText.innerText = "The winner is: No one! It's a Tie!";
-        } else {
-            winText.innerText = "The winner is: you!";
-        }
-    } else if (userChoice === 2) {
-        if (computerChoice === 3) {
-            winText.innerText = "The winner is: the computer!";
-        } else if (computerChoice === 2) {
-            winText.innerText = "The winner is: No one! It's a Tie!";
-        } else {
-            winText.innerText = "The winner is: you!";
-        }
-    } else if (userChoice === 3) {
-        if (computerChoice === 1) {
-            winText.innerText = "The winner is: the computer!";
-        } else if (computerChoice === 3) {
-            winText.innerText = "The winner is: No one! It's a Tie!";
-        } else {
-            winText.innerText = "The winner is: you!";
+    if ((userChoice != "rock") && (userChoice != "paper") && (userChoice != "scissors")) {
+        winText.innerText = "Please enter 'Rock', 'paper' or 'scissors'";
+    } else {
+        if (userChoice === "rock") {
+            if (computerChoice === 2) {
+                winText.innerText = "The winner is: the computer!";
+            } else if (computerChoice === 1) {
+                winText.innerText = "The winner is: No one! It's a Tie!";
+            } else {
+                winText.innerText = "The winner is: you!";
+            }
+        } else if (userChoice === "paper") {
+            if (computerChoice === 3) {
+                winText.innerText = "The winner is: the computer!";
+            } else if (computerChoice === 2) {
+                winText.innerText = "The winner is: No one! It's a Tie!";
+            } else {
+                winText.innerText = "The winner is: you!";
+            }
+        } else if (userChoice === "scissors") {
+            if (computerChoice === 1) {
+                winText.innerText = "The winner is: the computer!";
+            } else if (computerChoice === 3) {
+                winText.innerText = "The winner is: No one! It's a Tie!";
+            } else {
+                winText.innerText = "The winner is: you!";
+            }
         }
     }
 }
