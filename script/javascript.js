ca16a2a3863fe73630559b5e255770caa7d46fc0
RafSwiggers <rafswiggers@gmail.com>
2019-06-17 14:35:44 +0200

    fix onkey listener

--- script/javascript.js
+++ script/javascript.js
@@ -18,7 +18,7 @@ function go() {
 }
 
 
-input.addEventListener("keyup", function(event) {
+document.getElementById("userInput").addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
         rockPaperScissors;
