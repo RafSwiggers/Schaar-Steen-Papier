efe48d38fa8b270d052be96f4326d39e501e45ed
RafSwiggers <rafswiggers@gmail.com>
2019-06-17 11:11:36 +0200

    add the RPS function

--- script/javascript.js
+++ script/javascript.js
@@ -1,4 +1,5 @@
 function start() {
+    //This function removes the starting button and brins out the buttons for the player to choose.
     firstDiv = document.getElementById("first-game-div");
     startDiv = document.getElementById("start-button-div");
     firstDiv.classList.remove("d-none");
@@ -18,6 +19,42 @@ function setvar(x) {
     userChoice = x;
 }
 
-function rockpaperscissors(i) {
+function rockPaperScissors(i) {
+    var winText = document.getElementById("end-text");
+    var winDiv = document.getElementById("end-div")
+    var computerDiv = document.getElementById("computer-choice");
+    winDiv.classList.remove("d-none");
+    winDiv.classList.add("d-flex");
     console.log(userChoice);
+    var computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+
+    }
+
+    console.log(computerChoice);
+    if (userChoice === 1) {
+        if (computerChoice === 2) {
+            winText.insertAdjacentText("beforeend", " the computer!");
+        } else if (computerChoice === 1) {
+            winText.insertAdjacentText("beforeend", " No one! It's a Tie!");
+        } else {
+            winText.insertAdjacentText("beforeend", " you!");
+        }
+    } else if (userChoice === 2) {
+        if (computerChoice === 3) {
+            winText.insertAdjacentText("beforeend", " the computer!");
+        } else if (computerChoice === 2) {
+            winText.insertAdjacentText("beforeend", " No one! It's a Tie!");
+        } else {
+            winText.insertAdjacentText("beforeend", " you!");
+        }
+    } else if (userChoice === 3) {
+        if (computerChoice === 1) {
+            winText.insertAdjacentText("beforeend", " the computer!");
+        } else if (computerChoice === 3) {
+            winText.insertAdjacentText("beforeend", " No one! It's a Tie!");
+        } else {
+            winText.insertAdjacentText("beforeend", " you!");
+        }
+    }
 }
